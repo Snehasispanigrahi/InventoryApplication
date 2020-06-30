79dd0a59186ff4b1859658874ab1351d017b29ab
snehashishpanigrahi <panigrahisnehasis@gmail.com>
2020-06-30 23:06:45 +0530

    Initial Commit

---
+++ src/main/java/com/faltukart/inventory/cotroller/Checkout.java
@@ -0,0 +1,10 @@
+package com.faltukart.inventory.cotroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("checkout")
+public class Checkout {
+
+}
---
+++ src/test/java/com/faltukart/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faltukart.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
